bb9ddc9adf3cd0a8eff55b06de39b7e54b9aaa15
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-11-01 20:55:08 -0300

    add controller e rotas separada relação schema bd

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./http/routes";
 
 export const app = fastify();
 
-const prisma = new PrismaClient()
-
-prisma.user.create({
-    data:{
-        name: 'Luiz Prosdoskimi',
-        email: 'luiz@luiz.com'
-    }
-})
+app.register(appRoutes)
