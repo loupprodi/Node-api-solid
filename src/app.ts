1fc80014c9afe373c9f16625f28995a018441e0c
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-11-03 20:44:11 -0300

    add erroHandler ao app & error in controller register

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes)
+
+
+//colocar underline ( _ ) nos parametros nao usados dentro do handler
+app.setErrorHandler((error, _, reply) => {
+    if(error instanceof ZodError){
+        return reply.status(400).send({message: 'validation error', issues: error.format()})
+    }
+    if(env.NODE_ENV !== 'production'){
+        console.error(error)
+    } else{
+        //todo here we should log to an exeternal tool like datadog/newrelic/sentry
+    }
+
+    return reply.status(500).send({message: 'Internal Server Error'})
+})
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -23,7 +23,8 @@ export async function register(req : FastifyRequest, reply : FastifyReply) {
     if(error instanceof UserAlreadyExistsError){
         return reply.status(409).send({message: error.message})
     }
-    return reply.status(500).send() // TODO fix me
+
+    throw error
 }
 return reply.status(201).send()
 }
