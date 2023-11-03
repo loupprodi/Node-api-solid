329426708772162f9469e272a65d914d7fb3c61e
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-11-03 19:34:27 -0300

    add class repository para inversão de dependencia

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,8 @@
 
 import { FastifyReply, FastifyRequest} from 'fastify'
 import {z } from 'zod'
-import { registerUseCase } from '@/use-cases/register'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 
 export async function register(req : FastifyRequest, reply : FastifyReply) {
     const registerBodySchema = z.object({
@@ -12,9 +13,12 @@ export async function register(req : FastifyRequest, reply : FastifyReply) {
     const { name, email, password } = registerBodySchema.parse(req.body)
 
    try {
-    await registerUseCase({name, email, password})
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUsecase = new RegisterUseCase(prismaUsersRepository)
+
+    await registerUsecase.execute({name, email, password})
    } catch (error) {
     return reply.status(409).send()
    }
-    return reply.status(201).send()
+   return reply.status(201).send()
 }
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,4 @@
 import { prisma } from "@/lib/prisma"
-import { PrismaUsersRepository } from "@/repositories/prisma-users-repository"
 import { hash } from "bcryptjs"
 
 interface RegisterUseCaseRequestI{
@@ -8,22 +7,28 @@ interface RegisterUseCaseRequestI{
     password:string
 }
 
-export async function registerUseCase({name, email, password}: RegisterUseCaseRequestI){
-    const password_hash = await hash(password, 6)
+//SOLID -
+// D- Dependecy Inversion principle
+export class RegisterUseCase{
+    constructor(private usersRepository: any ){}
 
-    const userWithSameEmail = await prisma.user.findUnique({
-        where:{
-            email,
+   async execute({name, email, password}: RegisterUseCaseRequestI){
+        const password_hash = await hash(password, 6)
+    
+        const userWithSameEmail = await prisma.user.findUnique({
+            where:{
+                email,
+            }
+        })
+    
+        if(userWithSameEmail){
+            throw new Error("Email already exist. ")
         }
-    })
-
-    if(userWithSameEmail){
-        throw new Error("Email already exist. ")
+        // const prismaUsersRepository = new PrismaUsersRepository()
+    
+        await this.usersRepository.create({
+            name, email, password_hash
+        })
     }
-
-    const prismaUsersRepository = new PrismaUsersRepository()
-
-    await prismaUsersRepository.create({
-        name, email, password_hash
-    })
 }
+
