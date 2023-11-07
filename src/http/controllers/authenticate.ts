7694ac0b0b11fcf3854415a27c2ff36f4fc7571d
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-11-07 18:24:23 -0300

    facttory pattern in useCase to instance repos

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -4,6 +4,7 @@ import {z } from 'zod'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 
 export async function authenticate(req : FastifyRequest, reply : FastifyReply) {
@@ -14,9 +15,8 @@ export async function authenticate(req : FastifyRequest, reply : FastifyReply) {
     const { email, password } = authenticateBodySchema.parse(req.body)
 
    try {
-    const usersRepository = new PrismaUsersRepository()
-    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
-
+    //factory pattern
+    const authenticateUseCase = makeAuthenticateUseCase()
     await authenticateUseCase.execute({email, password})
    } catch (error) {
     if(error instanceof InvalidCredentialsError){
